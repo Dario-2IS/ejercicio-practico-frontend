7a4b2c4f3df7d013685f487a9044430b3704118f
Fresco <fresco@Frescos-MacBook-Air.local>
2025-05-25 19:21:56 -0500

    Depuracion de elementos.

--- src/app/layout/components/layout/layout.component.spec.ts
+++ src/app/layout/components/layout/layout.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LayoutComponent } from './layout.component';
+import { TopbarComponent } from '../topbar/topbar.component';
+import { MenuComponent } from '../menu/menu.component';
+import { RouterOutlet } from '@angular/router';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,16 +10,33 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LayoutComponent]
-    })
-    .compileComponents();
+      imports: [RouterOutlet, TopbarComponent, MenuComponent],
+      declarations: [LayoutComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the layout component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the topbar component', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-topbar')).toBeTruthy();
+  });
+
+  it('should render the menu component', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-menu')).toBeTruthy();
+  });
+
+  it('should contain a router outlet', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
 });
+
+// We recommend installing an extension to run jest tests.
